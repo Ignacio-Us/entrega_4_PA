4f42791b72e848c3695904561b378b762f8580ba
Ignacio-Us <i.uribe01@ufromail.cl>
2023-12-08 17:23:33 -0300

    Creacion del repositorio

---
+++ src/main/java/steam/teamb/socialNetwork/lup/LupApplication.java
@@ -0,0 +1,13 @@
+package steam.teamb.socialNetwork.lup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/steam/teamb/socialNetwork/lup/LupApplicationTests.java
@@ -0,0 +1,13 @@
+package steam.teamb.socialNetwork.lup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
