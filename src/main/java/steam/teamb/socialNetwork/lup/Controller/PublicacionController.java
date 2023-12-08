1e77eb52d04f194713377669226f21711e99e031
Ignacio-Us <i.uribe01@ufromail.cl>
2023-12-08 20:57:15 -0300

    Modificacion al archivo pom.xml
    Se agrego la dependencia Spring Data JPA

--- src/main/java/steam/teamb/socialNetwork/lup/Controller/PublicacionController.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Controller/PublicacionController.java
@@ -1,4 +1,10 @@
 package steam.teamb.socialNetwork.lup.Controller;
 
-public class PublicacionController {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import steam.teamb.socialNetwork.lup.Model.Entity.Publicacion;
+
+@Repository
+public interface PublicacionController extends JpaRepository<Publicacion, Long> {
+    
 }
