3472c8b0218a7ca3d3cfcc2af66efc177d2d5393
Ignacio-Us <i.uribe01@ufromail.cl>
2023-12-08 21:56:40 -0300

    Modificaciones al pom y PublicacionService
    Se añadio la dependencia de lombok con tal generar un codigo mas limpio.

--- src/main/java/steam/teamb/socialNetwork/lup/Model/Publicacion.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Model/Publicacion.java
@@ -1,44 +1,21 @@
 package steam.teamb.socialNetwork.lup.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Publicacion {
 
+    @Id
     private long id;
 
     private String titulo;
 
     private String contenido;
-
-    public Publicacion(long id, String titulo, String contenido) {
-        this.id = id;
-        this.titulo = titulo;
-        this.contenido = contenido;
-    }
-
-    public Publicacion(){
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getContenido() {
-        return contenido;
-    }
-
-    public void setContenido(String contenido) {
-        this.contenido = contenido;
-    }
 }
--- src/main/java/steam/teamb/socialNetwork/lup/Service/PublicacionService.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Service/PublicacionService.java
@@ -1,7 +1,24 @@
 package steam.teamb.socialNetwork.lup.Service;
 
-public interface PublicacionService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import steam.teamb.socialNetwork.lup.Model.Publicacion;
+import steam.teamb.socialNetwork.lup.Repository.PublicacionRepository;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+@Service
+public class PublicacionService {
 
+    @Autowired
+    PublicacionRepository publicacionRepository;
+
+    public ArrayList<Publicacion> obtenerPublicacion(){
+        return (ArrayList<Publicacion>) publicacionRepository.findAll();
+    }
+
+    public Publicacion guardarPublicacion(Publicacion publicacion){
+        return publicacionRepository.save(publicacion);
+    }
 }
