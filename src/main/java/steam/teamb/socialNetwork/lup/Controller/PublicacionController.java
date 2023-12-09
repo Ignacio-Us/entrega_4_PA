e096bda6c402c9c0accb137588c033529d396db0
Ignacio-Us <i.uribe01@ufromail.cl>
2023-12-09 16:09:57 -0300

    Modificacion de application.properties y agregacion de funciones para la publicacion
    Se agrego la propiedad databse-plataform.
    
    Se agregaron las funciones crear y obtener a los servicio de publicacion
    
    Se agregaron los controladores para crear y mostrar Publicacion

--- src/main/java/steam/teamb/socialNetwork/lup/Controller/PublicacionController.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Controller/PublicacionController.java
@@ -1,8 +1,33 @@
 package steam.teamb.socialNetwork.lup.Controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import steam.teamb.socialNetwork.lup.Model.Publicacion;
+import steam.teamb.socialNetwork.lup.Service.PublicacionService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/publicacion")
+@RequiredArgsConstructor
 public class PublicacionController{
 
+    private final PublicacionService publicacionService;
+
+    @PostMapping("/creando-publicacion")
+    public String crearPublicacion(@RequestBody Publicacion publicacion){
+        publicacionService.crearPublicacion(publicacion);
+        return "Publicacion creada";
+    }
+
+    @GetMapping("/mostrando-publicaciones")
+    public List<Publicacion> mostrarPublicacion(){
+        return publicacionService.obtenerPublicacion();
+    }
+
+    @PutMapping("/editando-publicacion")
+    public String editarPublicacion(@RequestBody Publicacion publicacion){
+
+        return "Publicacion editada";
+    }
 }
--- src/main/java/steam/teamb/socialNetwork/lup/Model/Publicacion.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Model/Publicacion.java
@@ -1,7 +1,6 @@
 package steam.teamb.socialNetwork.lup.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(schema = "publicacion")
 public class Publicacion {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String titulo;
-
     private String contenido;
 }
