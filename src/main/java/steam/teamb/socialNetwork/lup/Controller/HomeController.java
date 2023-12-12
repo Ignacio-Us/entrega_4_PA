b5ed524282f66c2f150da16d8b682975698163cc
diego lopez <hilixgremory@gmail.com>
2023-12-12 00:57:52 -0300

    se agrego HomeController
    para redireccionar al html home-social-network

--- src/main/java/steam/teamb/socialNetwork/lup/Controller/HomeController.java
+++ src/main/java/steam/teamb/socialNetwork/lup/Controller/HomeController.java
@@ -3,12 +3,11 @@ package steam.teamb.socialNetwork.lup.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class IndexController {
-    @GetMapping(value = "/posteo")
-    public String index(){
-        return "user-publicacion";
+public class HomeController {
+
+    @GetMapping(value = "/home")
+    public String verHome(){
+        return "home-social-network";
     }
-    
 }
